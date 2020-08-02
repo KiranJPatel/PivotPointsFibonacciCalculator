981023ca7695b267cc7e7f62610c9f9ee146c3cd
Kiran Patel <kiranpatel.it@gmail.com>
2020-08-02 21:42:44 +0530

    Add files via upload

---
+++ PivotPointsFibonacci.cs
@@ -0,0 +1,196 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PivotPointsFibonacciCalculator
+{
+    public class PivotPoints
+    {
+        [Display(Name = "High Price")]
+        public decimal HighPrice { get; set; }
+
+        [Display(Name = "Low Price")]
+        public decimal LowPrice { get; set; }
+
+        [Display(Name = "Opening Price")]
+        public decimal OpeningPrice { get; set; }
+
+        [Display(Name = "Closing Price")]
+        public decimal ClosingPrice { get; set; }
+
+        [Display(Name = "Classic Resistance 1")]
+        public decimal ClassicResistance1 { get; set; }
+
+        [Display(Name = "Classic Resistance 2")]
+        public decimal ClassicResistance2 { get; set; }
+
+        [Display(Name = "Classic Resistance 3")]
+        public decimal ClassicResistance3 { get; set; }
+
+        [Display(Name = "Classic Pivot Point")]
+        public decimal ClassicPivotPoint { get; set; }
+
+        [Display(Name = "Classic Support 1")]
+        public decimal ClassicSupport1 { get; set; }
+
+        [Display(Name = "Classic Support 2")]
+        public decimal ClassicSupport2 { get; set; }
+
+        [Display(Name = "Classic Support 3")]
+        public decimal ClassicSupport3 { get; set; }
+
+        [Display(Name = "Woodie Resistance 1")]
+        public decimal WoodieResistance1 { get; set; }
+
+        [Display(Name = "Woodie Resistance 2")]
+        public decimal WoodieResistance2 { get; set; }
+
+        [Display(Name = "Woodie Pivot Point")]
+        public decimal WoodiePivotPoint { get; set; }
+
+        [Display(Name = "Woodie Support 1")]
+        public decimal WoodieSupport1 { get; set; }
+
+        [Display(Name = "Woodie Support 2")]
+        public decimal WoodieSupport2 { get; set; }
+
+        [Display(Name = "Camarilla Resistance 1")]
+        public decimal CamarillaResistance1 { get; set; }
+
+        [Display(Name = "Camarilla Resistance 2")]
+        public decimal CamarillaResistance2 { get; set; }
+
+        [Display(Name = "Camarilla Resistance 3")]
+        public decimal CamarillaResistance3 { get; set; }
+
+        [Display(Name = "Camarilla Resistance 4")]
+        public decimal CamarillaResistance4 { get; set; }
+
+        [Display(Name = "Camarilla Support 1")]
+        public decimal CamarillaSupport1 { get; set; }
+
+        [Display(Name = "Camarilla Support 2")]
+        public decimal CamarillaSupport2 { get; set; }
+
+        [Display(Name = "Camarilla Support 3")]
+        public decimal CamarillaSupport3 { get; set; }
+
+        [Display(Name = "Camarilla Support 4")]
+        public decimal CamarillaSupport4 { get; set; }
+
+        [Display(Name = "DeMark Resistance")]
+        public decimal DeMarkResistance { get; set; }
+
+        [Display(Name = "DeMark Support")]
+        public decimal DeMarkSupport { get; set; }
+    }
+
+    public class FibonacciRetracements
+    {
+        [Display(Name = "High Price")]
+        public decimal HighPrice { get; set; }
+
+        [Display(Name = "Low Price")]
+        public decimal LowPrice { get; set; }
+
+        [Display(Name = "Target High Price")]
+        public decimal TargetHighPrice { get; set; }
+
+        [Display(Name = "Target Low Price")]
+        public decimal TargetLowPrice { get; set; }
+
+        [Display(Name = "Uptrend Retracements1 (23.6%)")]
+        public decimal UptrendRetracements1 { get; set; }
+
+        [Display(Name = "Uptrend Retracements2 (38.2%)")]
+        public decimal UptrendRetracements2 { get; set; }
+
+        [Display(Name = "Uptrend Retracements3 (50%)")]
+        public decimal UptrendRetracements3 { get; set; }
+
+        [Display(Name = "Uptrend Retracements4 (61.8%)")]
+        public decimal UptrendRetracements4 { get; set; }
+
+        [Display(Name = "Uptrend Retracement5 (76.4%)")]
+        public decimal UptrendRetracements5 { get; set; }
+
+        [Display(Name = "Uptrend Retracements6 (100%)")]
+        public decimal UptrendRetracements6 { get; set; }
+
+        [Display(Name = "Uptrend Retracements7 (138.2%)")]
+        public decimal UptrendRetracements7 { get; set; }
+
+        [Display(Name = "Uptrend Extentions1 (261.8%)")]
+        public decimal UptrendExtentions1 { get; set; }
+
+        [Display(Name = "Uptrend Extentions2 (200%)")]
+        public decimal UptrendExtentions2 { get; set; }
+
+        [Display(Name = "Uptrend Extentions3 (161.8%)")]
+        public decimal UptrendExtentions3 { get; set; }
+
+        [Display(Name = "Uptrend Extentions4 (138.2%)")]
+        public decimal UptrendExtentions4 { get; set; }
+
+        [Display(Name = "Uptrend Extentions5 (100%)")]
+        public decimal UptrendExtentions5 { get; set; }
+
+        [Display(Name = "Uptrend Extentions6 (61.8%)")]
+        public decimal UptrendExtentions6 { get; set; }
+
+        [Display(Name = "Uptrend Extentions7 (50%)")]
+        public decimal UptrendExtentions7 { get; set; }
+
+        [Display(Name = "Uptrend Extentions8 (38.2%)")]
+        public decimal UptrendExtentions8 { get; set; }
+
+        [Display(Name = "Uptrend Extentions9 (23.6%)")]
+        public decimal UptrendExtentions9 { get; set; }
+
+        [Display(Name = "Downtrend Retracements1 (23.6%)")]
+        public decimal DowntrendRetracements1 { get; set; }
+
+        [Display(Name = "Downtrend Retracements2 (38.2%)")]
+        public decimal DowntrendRetracements2 { get; set; }
+
+        [Display(Name = "Downtrend Retracements3 (50%)")]
+        public decimal DowntrendRetracements3 { get; set; }
+
+        [Display(Name = "Downtrend Retracements4 (61.8%)")]
+        public decimal DowntrendRetracements4 { get; set; }
+
+        [Display(Name = "Downtrend Retracements5 (76.4%)")]
+        public decimal DowntrendRetracements5 { get; set; }
+
+        [Display(Name = "Downtrend Retracements6 (100%)")]
+        public decimal DowntrendRetracements6 { get; set; }
+
+        [Display(Name = "Downtrend Retracements7 (138.2%)")]
+        public decimal DowntrendRetracements7 { get; set; }
+
+        [Display(Name = "Downtrend Extentions1 (261.8%)")]
+        public decimal DowntrendExtentions1 { get; set; }
+
+        [Display(Name = "Downtrend Extentions2 (200%)")]
+        public decimal DowntrendExtentions2 { get; set; }
+
+        [Display(Name = "Downtrend Extentions3 (161.8%)")]
+        public decimal DowntrendExtentions3 { get; set; }
+
+        [Display(Name = "Downtrend Extentions4 (138.2%)")]
+        public decimal DowntrendExtentions4 { get; set; }
+
+        [Display(Name = "Downtrend Extentions5 (100%)")]
+        public decimal DowntrendExtentions5 { get; set; }
+
+        [Display(Name = "Downtrend Extentions6 (61.8%)")]
+        public decimal DowntrendExtentions6 { get; set; }
+
+        [Display(Name = "Downtrend Extentions7 (50%)")]
+        public decimal DowntrendExtentions7 { get; set; }
+
+        [Display(Name = "Downtrend Extentions8 (38.2%)")]
+        public decimal DowntrendExtentions8 { get; set; }
+
+        [Display(Name = "Downtrend Extentions9 (23.6%)")]
+        public decimal DowntrendExtentions9 { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace PivotPointsFibonacciCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal dcOpeningPrice = 0;
+            decimal dcHighPrice = 0;
+            decimal dcLowPrice = 0;
+            decimal dcClosingPrice = 0;
+            Console.WriteLine("Please Enter Opening Price:");
+            if (!(decimal.TryParse(Console.ReadLine(), out dcOpeningPrice)))
+            {
+                Console.WriteLine("Invalid Opening Price; please try again!");
+            }
+            Console.WriteLine("Please Enter High Price:");
+            if (!(decimal.TryParse(Console.ReadLine(), out dcHighPrice)))
+            {
+                Console.WriteLine("Invalid High Price; please try again!");
+            }
+            Console.WriteLine("Please Enter Low Price:");
+            if (!(decimal.TryParse(Console.ReadLine(), out dcLowPrice)))
+            {
+                Console.WriteLine("Invalid Low Price; please try again!");
+            }
+            Console.WriteLine("Please Enter Closing Price:");
+            if (!(decimal.TryParse(Console.ReadLine(), out dcClosingPrice)))
+            {
+                Console.WriteLine("Invalid Closing Price; please try again!");
+            }
+
+
+            Console.WriteLine("\n#########################################################################");
+            Console.WriteLine("\nCalculated Pivot Points are:");
+            PivotPoints objPivotPoints = CalculatePivotPoints(dcOpeningPrice, dcHighPrice, dcLowPrice, dcClosingPrice);
+            string sPropertyName = string.Empty;
+            object objPropertyValue = null;
+            foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(objPivotPoints))
+            {
+                sPropertyName = descriptor.Name;
+                objPropertyValue = descriptor.GetValue(objPivotPoints);
+                Console.WriteLine("{0}: {1}", sPropertyName, Math.Round(Convert.ToDecimal(objPropertyValue), 2));
+            }
+
+            Console.WriteLine("\n#########################################################################");
+            Console.WriteLine("\nCalculated Fibonacci Retracements are:");
+           FibonacciRetracements objFibonacciRetracements = CalculateFibonacciRetracements(dcHighPrice, dcLowPrice);
+            foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(objPivotPoints))
+            {
+                sPropertyName = descriptor.Name;
+                objPropertyValue = descriptor.GetValue(objPivotPoints);
+                Console.WriteLine("{0}: {1}", sPropertyName, Math.Round(Convert.ToDecimal(objPropertyValue), 2));
+            }
+
+            Console.WriteLine("\n#########################################################################");
+
+            Console.ReadKey();
+        }
+
+        static PivotPoints CalculatePivotPoints(decimal dcOpeningPrice, decimal dcHighPrice, decimal dcLowPrice, decimal dcClosingPrice)
+        {
+            PivotPoints objPivotPoints = new PivotPoints();
+            try
+            {
+                objPivotPoints.OpeningPrice = dcOpeningPrice;
+                objPivotPoints.HighPrice = dcHighPrice;
+                objPivotPoints.LowPrice = dcLowPrice;
+                objPivotPoints.ClosingPrice = dcClosingPrice;
+
+
+                decimal dcAveragePrice = ((objPivotPoints.HighPrice + objPivotPoints.LowPrice + objPivotPoints.ClosingPrice) / 3);
+                objPivotPoints.ClassicResistance1 = ((2 * dcAveragePrice) - objPivotPoints.LowPrice);
+                objPivotPoints.ClassicSupport1 = ((2 * dcAveragePrice) - objPivotPoints.HighPrice);
+                objPivotPoints.ClassicResistance2 = dcAveragePrice + (objPivotPoints.HighPrice - objPivotPoints.LowPrice);
+                objPivotPoints.ClassicSupport2 = dcAveragePrice - (objPivotPoints.HighPrice - objPivotPoints.LowPrice);
+                objPivotPoints.ClassicResistance3 = objPivotPoints.HighPrice + 2 * (dcAveragePrice - objPivotPoints.LowPrice);
+                objPivotPoints.ClassicSupport3 = objPivotPoints.LowPrice - 2 * (objPivotPoints.HighPrice - dcAveragePrice);
+                objPivotPoints.ClassicPivotPoint = dcAveragePrice;
+
+                objPivotPoints.WoodiePivotPoint = (objPivotPoints.HighPrice + objPivotPoints.LowPrice + (2 * objPivotPoints.ClosingPrice)) / 4;
+                objPivotPoints.WoodieResistance1 = (2 * objPivotPoints.WoodiePivotPoint) - objPivotPoints.LowPrice;
+                objPivotPoints.WoodieSupport1 = (2 * objPivotPoints.WoodiePivotPoint) - objPivotPoints.HighPrice;
+                objPivotPoints.WoodieResistance2 = objPivotPoints.WoodiePivotPoint + (objPivotPoints.HighPrice - objPivotPoints.LowPrice);
+                objPivotPoints.WoodieSupport2 = objPivotPoints.WoodiePivotPoint - (objPivotPoints.HighPrice - objPivotPoints.LowPrice);
+
+                objPivotPoints.CamarillaResistance1 = objPivotPoints.ClosingPrice + ((objPivotPoints.HighPrice - objPivotPoints.LowPrice) * Convert.ToDecimal(1.0833));
+                objPivotPoints.CamarillaSupport1 = objPivotPoints.ClosingPrice - ((objPivotPoints.HighPrice - objPivotPoints.LowPrice) * Convert.ToDecimal(1.0833));
+                objPivotPoints.CamarillaResistance2 = objPivotPoints.ClosingPrice + ((objPivotPoints.HighPrice - objPivotPoints.LowPrice) * Convert.ToDecimal(1.1666));
+                objPivotPoints.CamarillaSupport2 = objPivotPoints.ClosingPrice - ((objPivotPoints.HighPrice - objPivotPoints.LowPrice) * Convert.ToDecimal(1.1666));
+                objPivotPoints.CamarillaResistance3 = objPivotPoints.ClosingPrice + ((objPivotPoints.HighPrice - objPivotPoints.LowPrice) * Convert.ToDecimal(1.2500));
+                objPivotPoints.CamarillaSupport3 = objPivotPoints.ClosingPrice - ((objPivotPoints.HighPrice - objPivotPoints.LowPrice) * Convert.ToDecimal(1.2500));
+                objPivotPoints.CamarillaResistance4 = objPivotPoints.ClosingPrice + ((objPivotPoints.HighPrice - objPivotPoints.LowPrice) * Convert.ToDecimal(1.5000));
+                objPivotPoints.CamarillaSupport4 = objPivotPoints.ClosingPrice - ((objPivotPoints.HighPrice - objPivotPoints.LowPrice) * Convert.ToDecimal(1.5000));
+                decimal dcDeMarkFactor = 0;
+                if (objPivotPoints.ClosingPrice < objPivotPoints.OpeningPrice)
+                {
+                    dcDeMarkFactor = (objPivotPoints.HighPrice + (objPivotPoints.LowPrice * 2) + objPivotPoints.ClosingPrice);
+                }
+                else if (objPivotPoints.ClosingPrice > objPivotPoints.OpeningPrice)
+                {
+                    dcDeMarkFactor = ((objPivotPoints.HighPrice * 2) + objPivotPoints.LowPrice + objPivotPoints.ClosingPrice);
+                }
+                else if (objPivotPoints.ClosingPrice == objPivotPoints.OpeningPrice)
+                {
+                    dcDeMarkFactor = (objPivotPoints.HighPrice + objPivotPoints.LowPrice + (objPivotPoints.ClosingPrice * 2));
+                }
+                objPivotPoints.DeMarkResistance = (dcDeMarkFactor / 2) - objPivotPoints.LowPrice;
+                objPivotPoints.DeMarkSupport = (dcDeMarkFactor / 2) - objPivotPoints.HighPrice;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error occurred while calculating pivot points:" + ex.Message);
+            }
+
+            return objPivotPoints;
+        }
+
+        static FibonacciRetracements CalculateFibonacciRetracements(decimal dcHighPrice, decimal dcLowPrice, decimal dbPercentageAdjustment = 0)
+        {
+            decimal dcHighLowDiff = 0;
+            FibonacciRetracements objFibonacciRetracements = new FibonacciRetracements();
+            try
+            {
+                objFibonacciRetracements.HighPrice = dcHighPrice;
+                objFibonacciRetracements.LowPrice = dcLowPrice;
+
+                dcHighLowDiff = objFibonacciRetracements.HighPrice - objFibonacciRetracements.LowPrice;
+                objFibonacciRetracements.UptrendRetracements1 = objFibonacciRetracements.HighPrice - (dcHighLowDiff * Convert.ToDecimal(0.236));
+                objFibonacciRetracements.UptrendRetracements2 = objFibonacciRetracements.HighPrice - (dcHighLowDiff * Convert.ToDecimal(0.382));
+                objFibonacciRetracements.UptrendRetracements3 = objFibonacciRetracements.HighPrice - (dcHighLowDiff * Convert.ToDecimal(0.5));
+                objFibonacciRetracements.UptrendRetracements4 = objFibonacciRetracements.HighPrice - (dcHighLowDiff * Convert.ToDecimal(0.618));
+                objFibonacciRetracements.UptrendRetracements5 = objFibonacciRetracements.HighPrice - (dcHighLowDiff * Convert.ToDecimal(0.764));
+                objFibonacciRetracements.UptrendRetracements6 = objFibonacciRetracements.HighPrice - dcHighLowDiff;
+                objFibonacciRetracements.UptrendRetracements7 = objFibonacciRetracements.HighPrice - (dcHighLowDiff * Convert.ToDecimal(1.382));
+
+                objFibonacciRetracements.TargetHighPrice = dbPercentageAdjustment != 0 ? objFibonacciRetracements.HighPrice + (objFibonacciRetracements.HighPrice + Convert.ToDecimal(dbPercentageAdjustment / 100)) : 0;
+                decimal dcExtentionsBase = (objFibonacciRetracements.TargetHighPrice > 0 && objFibonacciRetracements.HighPrice != objFibonacciRetracements.TargetHighPrice ? objFibonacciRetracements.TargetHighPrice : objFibonacciRetracements.HighPrice);
+                objFibonacciRetracements.UptrendExtentions1 = dcExtentionsBase + (dcHighLowDiff * Convert.ToDecimal(2.618));
+                objFibonacciRetracements.UptrendExtentions2 = dcExtentionsBase + (dcHighLowDiff * Convert.ToDecimal(2));
+                objFibonacciRetracements.UptrendExtentions3 = dcExtentionsBase + (dcHighLowDiff * Convert.ToDecimal(1.618));
+                objFibonacciRetracements.UptrendExtentions4 = dcExtentionsBase + (dcHighLowDiff * Convert.ToDecimal(1.382));
+                objFibonacciRetracements.UptrendExtentions5 = dcExtentionsBase + dcHighLowDiff;
+                objFibonacciRetracements.UptrendExtentions6 = dcExtentionsBase + (dcHighLowDiff * Convert.ToDecimal(0.618));
+                objFibonacciRetracements.UptrendExtentions7 = dcExtentionsBase + (dcHighLowDiff * Convert.ToDecimal(0.5));
+                objFibonacciRetracements.UptrendExtentions8 = dcExtentionsBase + (dcHighLowDiff * Convert.ToDecimal(0.382));
+                objFibonacciRetracements.UptrendExtentions9 = dcExtentionsBase + (dcHighLowDiff * Convert.ToDecimal(0.236));
+
+                objFibonacciRetracements.DowntrendRetracements1 = objFibonacciRetracements.LowPrice + (dcHighLowDiff * Convert.ToDecimal(0.236));
+                objFibonacciRetracements.DowntrendRetracements2 = objFibonacciRetracements.LowPrice + (dcHighLowDiff * Convert.ToDecimal(0.382));
+                objFibonacciRetracements.DowntrendRetracements3 = objFibonacciRetracements.LowPrice + (dcHighLowDiff * Convert.ToDecimal(0.5));
+                objFibonacciRetracements.DowntrendRetracements4 = objFibonacciRetracements.LowPrice + (dcHighLowDiff * Convert.ToDecimal(0.618));
+                objFibonacciRetracements.DowntrendRetracements5 = objFibonacciRetracements.LowPrice + (dcHighLowDiff * Convert.ToDecimal(0.764));
+                objFibonacciRetracements.DowntrendRetracements6 = objFibonacciRetracements.LowPrice + dcHighLowDiff;
+                objFibonacciRetracements.DowntrendRetracements7 = objFibonacciRetracements.LowPrice + (dcHighLowDiff * Convert.ToDecimal(1.382));
+
+                objFibonacciRetracements.TargetLowPrice = dbPercentageAdjustment != 0 ? objFibonacciRetracements.LowPrice - (objFibonacciRetracements.LowPrice * Convert.ToDecimal(dbPercentageAdjustment / 100)) : 0;
+                dcExtentionsBase = (objFibonacciRetracements.TargetLowPrice > 0 && objFibonacciRetracements.LowPrice != objFibonacciRetracements.TargetLowPrice ? objFibonacciRetracements.TargetLowPrice : objFibonacciRetracements.LowPrice);
+                objFibonacciRetracements.DowntrendExtentions1 = dcExtentionsBase - (dcHighLowDiff * Convert.ToDecimal(2.618));
+                objFibonacciRetracements.DowntrendExtentions2 = dcExtentionsBase - (dcHighLowDiff * Convert.ToDecimal(2));
+                objFibonacciRetracements.DowntrendExtentions3 = dcExtentionsBase - (dcHighLowDiff * Convert.ToDecimal(1.618));
+                objFibonacciRetracements.DowntrendExtentions4 = dcExtentionsBase - (dcHighLowDiff * Convert.ToDecimal(1.382));
+                objFibonacciRetracements.DowntrendExtentions5 = dcExtentionsBase - dcHighLowDiff;
+                objFibonacciRetracements.DowntrendExtentions6 = dcExtentionsBase - (dcHighLowDiff * Convert.ToDecimal(0.618));
+                objFibonacciRetracements.DowntrendExtentions7 = dcExtentionsBase - (dcHighLowDiff * Convert.ToDecimal(0.5));
+                objFibonacciRetracements.DowntrendExtentions8 = dcExtentionsBase - (dcHighLowDiff * Convert.ToDecimal(0.382));
+                objFibonacciRetracements.DowntrendExtentions9 = dcExtentionsBase - (dcHighLowDiff * Convert.ToDecimal(0.236));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error occurred while calculating pivot points:" + ex.Message);
+            }
+            return objFibonacciRetracements;
+        }
+    }
+}
